11d4bd74d4302c8b72f5f91eaf3e6f7162546b04
Cyan244466666 <cammellryan@gmail.com>
2022-02-07 14:49:20 +0000

    Refactored VBO function.

--- src/gl_functions.cpp
+++ src/gl_functions.cpp
@@ -3,13 +3,14 @@
 // i) Create a new VBO on the GPU and bind it to GPU.
 // ii) Clear the binding point.
 // iii) Return new buffer object(s)' ID.
-GLuint vbo_set_and_clear(const GLfloat vertices[], const GLsizei num_of_buffers, const GLenum binding_point, const GLenum data_usage) {
+GLuint vbo_set_and_clear(const std::vector<GLfloat>& vertices, const GLsizei num_of_buffers, const GLenum binding_point, const GLenum data_usage) {
+
     // Create a new VBO on the GPU and bind it
     GLuint vbo_id = 0;
     glGenBuffers(num_of_buffers, &vbo_id);
     if (!vbo_id) throw std::exception();
     glBindBuffer(binding_point, vbo_id);
-    glBufferData(binding_point, sizeof(vertices), vertices, data_usage);
+    glBufferData(binding_point, vertices.size() * sizeof(vertices.at(0)), &vertices.at(0), data_usage);
     // Clear the buffer object and return ID:
     glBindBuffer(binding_point, 0);
     return vbo_id;
@@ -18,11 +19,11 @@ GLuint vbo_set_and_clear(const GLfloat vertices[], const GLsizei num_of_buffers,
 // i) Create a new VAO on the GPU and bind it to GPU.
 // ii) Assign layout of data (for VBO) to the VAO.
 // iii) Return new VAO's ID.
-GLuint vao_set_and_clear(const GLfloat vertices[], GLuint vbo_id, const GLsizei num_of_buffers, const GLenum binding_point,
+GLuint vao_set_and_clear(const std::vector<GLfloat>& vertices, GLuint vbo_id, const GLsizei num_of_buffers, const GLenum binding_point,
     const GLboolean normalise) {
     // Create a VAO with data from the VBO:
     GLuint vao_id = 0;            
-    GLsizei number_of_vertices = (sizeof(vertices) / sizeof(GLfloat)) / 3;
+    GLsizei number_of_vertices = vertices.size() / 3;
     glGenVertexArrays(num_of_buffers, &vao_id);
     if (!vao_id) throw std::exception();
     glBindVertexArray(vao_id);
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <SDL.h>
 #include <glew.h>
+#include <vector>
 
 const GLchar* vertex_shader_filepath = "Additional_Files/vertex_shader.txt";
 const GLchar* fragment_shader_filepath = "Additional_Files/fragment_shader.txt";
@@ -18,28 +19,47 @@ int main(int argc, char* argv[]) {
     if (glewInit() != GLEW_OK) throw std::exception(); 
 
     // Define vertices of triangle:
-    const GLfloat positions[]{
-        0.0f, 0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f
-    };
+    std::vector<GLfloat> positions;
+    positions.push_back(0.5f);
+    positions.push_back(0.5f);
+    positions.push_back(0.0f);
+    positions.push_back(-0.5f);
+    positions.push_back(-0.5f);
+    positions.push_back(0.0f);
+    positions.push_back(0.5f);
+    positions.push_back(-0.5f);
+    positions.push_back(0.0f);
 
+   /* const GLfloat positions[] = {
+  0.0f, 0.5f, 0.0f,
+  -0.5f, -0.5f, 0.0f,
+  0.5f, -0.5f, 0.0f
+    };*/
+    
+    /////////// VERTEX BUFFER OBJECT ///////////
     // Bind vertex data to GPU and store the buffer object(s)' ID:
     GLuint triangle_vbo_id = vbo_set_and_clear(positions, 1, GL_ARRAY_BUFFER, GL_STATIC_DRAW);
 
+
+    ///////////// VERTEX ARRAY OBJECT ///////////
     // Create a VAO and store layout of VBO:
     GLuint triangle_vao_id = vao_set_and_clear(positions, triangle_vbo_id, 1, GL_ARRAY_BUFFER, GL_FALSE);
+   
 
+    ///////////// VERTEX SHADER ///////////
     // Vertex Shader: Read source file, then generate and compile shader object:
     std::string vertex_shader_src_str = read_shader_file(vertex_shader_filepath);
     const GLchar* vertex_shader_src_char = vertex_shader_src_str.c_str();  // convert from string to const GLchar*
     GLuint vertex_shader_id = create_shader_object(GL_VERTEX_SHADER, 1, vertex_shader_src_char);
+ 
    
+    ///////////// FRAGMENT SHADER ///////////
     // Fragment Shader: Read source file, then generate and compile shader object:
     std::string fragment_shader_src_str = read_shader_file(fragment_shader_filepath);
     const GLchar* fragment_shader_src_char = fragment_shader_src_str.c_str();  // convert from string to const GLchar*
     GLuint fragment_shader_id = create_shader_object(GL_FRAGMENT_SHADER, 1, fragment_shader_src_char);
 
+    ///////////// SHADER PROGRAM ///////////
     // Create new shader program and attach shader objects:
     GLuint program_id = glCreateProgram();
     glAttachShader(program_id, vertex_shader_id);
@@ -54,17 +74,51 @@ int main(int argc, char* argv[]) {
     glDeleteShader(vertex_shader_id);
     glDetachShader(program_id, fragment_shader_id);
     glDeleteShader(fragment_shader_id);
+    
 
-    // Instruct OpenGL to use the shader program and the VAO:
-    glUseProgram(program_id);
-    glBindVertexArray(triangle_vao_id);
-    glDrawArrays(GL_TRIANGLES, 0, 3); // Draw 3 vertices (triangle)
-    // Reset the state:
-    glBindVertexArray(0);
-    glUseProgram(0);
+    ///////////// DRAW CALL ///////////
+    //// Instruct OpenGL to use the shader program and the VAO:
+    //glUseProgram(program_id);
+    //glBindVertexArray(triangle_vao_id);
+    //glDrawArrays(GL_TRIANGLES, 0, 3); // Draw 3 vertices (triangle)
+    //// Reset the state:
+    //glBindVertexArray(0);
+    //glUseProgram(0);
+    // Instruct OpenGL to use our shader program and our VAO
    
-    // Input Loop:
-    sdl_loop(window);
+   
+
+    ///////////// INPUT LOOP ///////////
+    SDL_Event event;
+    bool running = true;
+
+    while (running) {
+        change_window_colour(1.0f, 0.0f, 0.0f, 1.0f, window);
+       
+        glUseProgram(program_id);
+        glBindVertexArray(triangle_vao_id);
+
+        // Draw 3 vertices (a triangle)
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // Reset the state
+        glBindVertexArray(0);
+        glUseProgram(0);
+        SDL_GL_SwapWindow(window);
+        while (SDL_PollEvent(&event) != 0) {  // Check for user input
+            switch (event.type)
+            {
+            case SDL_QUIT:                    // Quit application
+                running = false;
+                break;
+            default:
+                break;
+            }
+        }
+    }
+
+    //// Input Loop:
+    //sdl_loop(window);
 
     return 0; // Exit
 }
