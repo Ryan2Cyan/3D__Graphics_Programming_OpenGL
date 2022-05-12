142ee70f6aa3c74fddce322e84d1f118503f3d0f
Ryan Bolson (s5326665) <s5326665@bournemouth.ac.uk>
2022-05-12 16:16:22 +0100

    Finished implementation of test.cpp to understand collision detection.

--- src/demos/test.cpp
+++ src/demos/test.cpp
@@ -3,11 +3,14 @@
 
 int main() {
 	
-	// Calculate Jlinear force ////////////////////////////////////////////////////////////////////::
+	// Calculate Jlinear force ////////////////////////////////////////////////////////////////////:
+	bool _stopped = false;
+	float delta_time = 0.05f;
 	float Jlinear = 0.0f;
 	float e = 0.5f;
-	float oneOverMass1 = 1.0f / 2.0f;
-	float oneOverMass2 = 1.0f / 2.0f;
+	float mass = 2.0f;
+	float oneOverMass1 = 1.0f / mass;
+	float oneOverMass2 = 1.0f / mass;
 	glm::vec3 vA = glm::vec3(0.0f, 20.0f, 0.0f);
 	glm::vec3 contactNormal = glm::vec3(0.0f, 1.0f, 0.0f);
 
@@ -28,8 +31,7 @@ int main() {
 	// Calculate Jangular force ////////////////////////////////////////////////////////////////////:
 	float Jangular = 0.0f;
 	float _bRadius = 2.0f;
-	float r1 = _bRadius;
-	float mass = 2.0f;
+	glm::vec3 r1 = _bRadius * glm::vec3(0.0f, 1.0f, 0.0f);
 	glm::vec3 vB = glm::vec3(0.0f, 0.0f, 0.0f);
 	glm::vec3 relativeVelocity = vA - vB;
 	glm::mat3 _R = glm::mat3(
@@ -81,15 +83,97 @@ int main() {
 	std::cout << contactForce.x << ", " << contactForce.y << ", " << contactForce.z << std::endl;
 	std::cout << std::endl;
 
-	// Calculate Contact force ////////////////////////////////////////////////////////////////////:
-	glm::vec3 contactForce;
-	glm::vec3 gravityFroce = glm::vec3(0.0f, -9.8f * mass, 0.0f);
-	glm::vec3 _force = glm::vec3(0.0f, -9.8f * mass, 0.0f);
+	// Calculate Forward Relative Velocity ////////////////////////////////////////////////////////////////////:
+	glm::vec3 forwardRelativeVelocity;
 
-	contactForce = -(_force)* contactNormal;;
+	float frv_pt1 = glm::dot(relativeVelocity, contactNormal);
+	glm::vec3 frv_pt2 = frv_pt1 * contactNormal;
+	forwardRelativeVelocity = relativeVelocity - frv_pt1;
 
 	// Print output:
-	std::cout << "Contact Force" << std::endl;
-	std::cout << contactForce.x << ", " << contactForce.y << ", " << contactForce.z << std::endl;
+	std::cout << "Forward Relative Velocity" << std::endl;
+	std::cout << relativeVelocity.x << ", " << relativeVelocity.y << ", " << relativeVelocity.z << std::endl;
+	std::cout << frv_pt1 << std::endl;
+	std::cout << frv_pt2.x << ", " << frv_pt2.y << ", " << frv_pt2.z << std::endl;
+	std::cout << forwardRelativeVelocity.x << ", " << forwardRelativeVelocity.y << ", " << forwardRelativeVelocity.z << std::endl;
+	std::cout << std::endl;
+
+	// Calculate Forward Relative Direction ////////////////////////////////////////////////////////////////////:
+	glm::vec3 forwardRelativeDirection = glm::vec3(0.0f, 0.0f, 0.0f);
+	
+	if (forwardRelativeVelocity != glm::vec3(0.0f, 0.0f, 0.0f))
+	{
+		forwardRelativeDirection = glm::normalize(forwardRelativeVelocity); // gets a normalized vector of the direction travelled perpendicular to the contact normal
+	}
+
+	// Print output:
+	std::cout << "Forward Relative Direction" << std::endl;
+	std::cout << forwardRelativeDirection.x << ", " << forwardRelativeDirection.y << ", " << forwardRelativeDirection.z << std::endl;
+	std::cout << std::endl;
+
+
+	// Calculate Friction Direction ////////////////////////////////////////////////////////////////////:
+	float mu = 0.5f;
+
+	glm::vec3 frictionDirection = forwardRelativeDirection * -1.0f;
+
+	// Print output:
+	std::cout << "Friction Direction" << std::endl;
+	std::cout << frictionDirection.x << ", " << frictionDirection.y << ", " << frictionDirection.z << std::endl;
+	std::cout << std::endl;
+
+	// Calculate Friction Force //////////////////////////////////////////////////////////////////////:
+	float frictForce_pt1 = glm::length(contactForce);
+	glm::vec3 frictionForce = frictionDirection * mu * frictForce_pt1; // friction = mu * normal force
+
+	// Print output:
+	std::cout << "Friction Force" << std::endl;
+	std::cout << frictForce_pt1 << std::endl;
+	std::cout << frictionForce.x << ", " << frictionForce.y << ", " << frictionForce.z << std::endl;
+	std::cout << std::endl;
+
+	// Applying Friction Force //////////////////////////////////////////////////////////////////////:
+
+	// Print output:
+	std::cout << "Applying Friction Force" << std::endl;
+	std::cout << glm::length(forwardRelativeVelocity) << std::endl;
+	std::cout << ((glm::length(frictionForce) / mass)* delta_time) << std::endl;
+	std::cout << glm::length(forwardRelativeVelocity) - ((glm::length(frictionForce) / mass)* delta_time) << std::endl;
+	
+	if (glm::length(forwardRelativeVelocity) - ((glm::length(frictionForce) / mass) * delta_time) > 0.0f) // Checks to see if the friction force would reverse the direction of travel
+	{
+		// AddForce(frictionForce);	// Adds the friction
+		std::cout << "Continue: Add Force: " << frictionForce.x << ", " << frictionForce.y << ", " << frictionForce.z << std::endl;
+	}
+	else
+	{
+		frictionForce = forwardRelativeVelocity * -1.0f; // Adds enough friction to stop the object
+		std::cout << "Stop: Add Force: " << frictionForce.x << ", " << frictionForce.y << ", " << frictionForce.z << std::endl;
+		// AddForce(frictionForce);
+		_stopped = true;
+	}
+
+	std::cout << std::endl;
+
+	// Calculating Torque //////////////////////////////////////////////////////////////////////:
+	glm::vec3 torque_pt1 = glm::cross(r1, contactForce);
+	glm::vec3 torque_pt2 = glm::cross(r1, frictionForce);
+	glm::vec3 tempTorque = torque_pt1 + torque_pt2;
+
+	// Print output:
+	std::cout << "Calculating Torque" << std::endl;
+	std::cout << torque_pt1.x << ", " << torque_pt1.y << ", " << torque_pt1.z << std::endl;
+	std::cout << torque_pt2.x << ", " << torque_pt2.y << ", " << torque_pt2.z << std::endl;
+	std::cout << tempTorque.x << ", " << tempTorque.y << ", " << tempTorque.z << std::endl;
+	std::cout << std::endl;
+
+	// Applying Torque //////////////////////////////////////////////////////////////////////:
+	glm::vec3 angular_momentum = glm::vec3(0.0f, 0.0f, 0.0f);
+	tempTorque.x -= angular_momentum.x * 20.0f;
+	tempTorque.z -= angular_momentum.z * 20.0f; // A damper to slow the rotation over time:
+
+	// Print output:
+	std::cout << "Applying Torque" << std::endl;
+	std::cout << tempTorque.x << ", " << tempTorque.y << ", " << tempTorque.z << std::endl;
 	std::cout << std::endl;
 }
